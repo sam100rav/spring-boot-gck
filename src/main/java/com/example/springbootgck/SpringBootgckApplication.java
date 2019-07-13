543826b6cf737ff62fc8a00740d0f7c1ee43c7cc
sam100rav <samantsinha@hotmail.com>
2019-07-13 15:28:44 +0530

    added hello world rest endpoint

---
+++ src/main/java/com/example/springbootgck/SpringBootgckApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootgckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootgckApplication.class, args);
+	}
+
+}
